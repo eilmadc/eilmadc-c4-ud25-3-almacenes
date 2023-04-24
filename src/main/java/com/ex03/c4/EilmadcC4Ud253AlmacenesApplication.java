91135741050caa2f6f008e665e1eaf539f42f53f
eilmadc@gmail.com <eilmadc@gmail.com>
2023-04-24 20:27:08 +0200

    Initial commit

---
+++ src/main/java/com/ex03/c4/EilmadcC4Ud253AlmacenesApplication.java
@@ -0,0 +1,13 @@
+package com.ex03.c4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EilmadcC4Ud253AlmacenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EilmadcC4Ud253AlmacenesApplication.class, args);
+	}
+
+}
